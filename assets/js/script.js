4fa0cea63d45408a1b74166bfb24c4a0c9e3f741
Nitin Mukesh <nitinmukes@gmail.com>
2020-06-13 00:31:32 +0800

    correct 12 noon block display and enahnce color coding logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,41 @@
 $(document).ready(function () {
+    // event listerner which will trigger save event handler whenever user click
+    //on save button.
     $(document).on("click", ".save-btn", saveEventDetails);
+    // need to refresh time and hour color code after each minute so that correct color
+    // code is visible and updated time is visible.
     setInterval(function () {
         renderTime();
         renderEventColorCode();
     }, 60000);
+    //register a handler to change color of corresponding save button if save needed
+    //whenever user clicks some key in any event block.
     $(document).on("keyup", ".event-block", changeSaveBtnColor);
     renderTime();
     renderEventColorCode();
+    // create an empty array to maintain each event block details and persist
+    // same in local storage.
     var eventBlockDetails = [];
     initEventBlocks();
     renderEventDetails();
 
+    /**
+     * based on current time, changes the background color of each event block
+     * hour.
+     */
     function renderEventColorCode() {
         $.each($(".event-block"), function (index, item) {
             //console.log(item);
             const blockHour = parseInt($(item).attr("data-time"));
             // console.log(typeof(blockHour));
             var currentTimeHour = parseInt(moment().format('h'));
-            if (currentTimeHour != 12 && moment().format('a') == "pm") {
+            if (12 != currentTimeHour && "pm" == moment().format('a')) {
                 currentTimeHour += 12;
             }
-            if (blockHour === currentTimeHour) {
+            else if (12 == currentTimeHour && "am" == moment().format('a')) {
+                currentTimeHour = 0;
+            }
+            if (blockHour == currentTimeHour) {
                 resetTimeClass(item, "present");
             }
             else if (blockHour < currentTimeHour) {
@@ -29,11 +44,14 @@ $(document).ready(function () {
             else {
                 resetTimeClass(item, "future");
             }
-            // console.log(currentTimeHour);
-            //console.log(typeof(currentTimeHour));
         });
     }
 
+    /**
+     * reset class which controls the background color of the item
+     * @param {*} item event detail block whose color need to be updated.
+     * @param {*} currentClass class which need to be added to item.
+     */
     function resetTimeClass(item, currentClass) {
         $(item).removeClass("present");
         $(item).removeClass("past");
@@ -41,6 +59,9 @@ $(document).ready(function () {
         $(item).addClass(currentClass);
     }
 
+    /**
+     * initializes the eventBlockDetails array from local storage.
+     */
     function initEventBlocks() {
         const storedEventBlocks = localStorage.getItem("eventBlockDetails");
         if (storedEventBlocks && storedEventBlocks.length) {
@@ -48,10 +69,18 @@ $(document).ready(function () {
         }
     }
 
+    /**
+     * renders current day, date and time
+     */
     function renderTime() {
         $("#current-date").text(moment().format('dddd, Do MMMM YYYY, h:mm a'));
     }
 
+    /**
+     * event handler function triggerred by click on save button
+     * to save event details in local storage. It either creates new entry or
+     * updates exisiting entry of evenBlockDetails array.
+     */
     function saveEventDetails() {
         const eventBlockEl = $(this).parent().parent().children("textarea")
         const eventDetails = eventBlockEl.val().trim();
@@ -76,13 +105,20 @@ $(document).ready(function () {
         $(this).removeClass("need-save");
     }
 
+    /**
+     * iterates over eventBlockDetails array and renders saved text in correspnding
+     * event block based on time hour.
+     */
     function renderEventDetails() {
         $.each(eventBlockDetails, function (index, item) {
             const timeHour = item.timeHour;
             $("#event-block-" + timeHour).val(item.eventDetails);
         });
     }
-
+    /**
+     * changes save button colour if user has entered some text in corresponding input block
+     * and if save need to be pressed to save the updated information for that block.
+     */
     function changeSaveBtnColor() {
         const saveNeeded = isSaveNeeded($(this).attr("data-time"), $(this).val());
         console.log(saveNeeded);
@@ -102,6 +138,11 @@ $(document).ready(function () {
 
     }
 
+    /**
+     * determined if save needed for current event block
+     * @param {*} hourNumber current block hour number
+     * @param {*} currentText current text present in event block
+     */
     function isSaveNeeded(hourNumber, currentText) {
         console.log(currentText);
         var saveNeeded = false;
@@ -118,8 +159,7 @@ $(document).ready(function () {
                 }
             }
         });
-        if(currentText.trim().length >0)
-        {
+        if (currentText.trim().length > 0) {
             saveNeeded = true;
         }
         return saveNeeded;
